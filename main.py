9248f5a1fc7966e8a6e26174c80a697f410ca602
Luka Skenderija <luka.skenderija@gmail.com>
2025-08-07 21:56:30 +0200

    Wrap to functions

--- main.py
+++ main.py
@@ -3,17 +3,28 @@ import pandas as pd
 # variables
 kurtosis_window = "180D"
 
-# preprocessing
-src = pd.read_csv('crsp.csv')
-src = src[['permno', 'ticker', 'comnam', 'date', 'prc', 'ret']]
-src['date'] = pd.to_datetime(src['date'], format='%Y%m%d')
-src = src.sort_values(by=['permno', 'date'])
+def load_csv(path):
+    src = pd.read_csv(path)
 
-# group by stock
-grouped = src.groupby('permno')
+    # preprocessing
+    src = src[['permno', 'ticker', 'comnam', 'date', 'prc', 'ret']]
+    src['date'] = pd.to_datetime(src['date'], format='%Y%m%d')
+    src = src.sort_values(by=['permno', 'date'])
 
-src['kurtosis'] = None 
+    return src
 
-for permno, group in src.groupby('permno'):
-    k = group.set_index('date')['ret'].rolling(window=kurtosis_window).kurt()
-    src.loc[group.index, 'kurtosis'] = k.values
+def calculate_kurtosis(frame: pd.DataFrame):
+    # group by stock
+    grouped = frame.groupby('permno')
+
+    # calculate curtiosis
+    frame['kurtosis'] = None 
+
+    for _, group in frame.groupby('permno'):
+        k = group.set_index('date')['ret'].rolling(window=kurtosis_window).kurt()
+        frame.loc[group.index, 'kurtosis'] = k.values
+
+    return frame
+
+# calculate kurtosis
+df = calculate_kurtosis(load_csv('crsp.csv'))
