77b06242f57f785851831334b4e05d8d32858138
Luka Skenderija <luka.skenderija@gmail.com>
2025-08-08 00:19:14 +0200

    Iteracija po kvantilima

--- main.py
+++ main.py
@@ -14,10 +14,6 @@ def load_csv(path):
     return src
 
 def calculate_kurtosis(frame: pd.DataFrame):
-    # group by stock
-    grouped = frame.groupby('permno')
-
-    # calculate curtiosis
     frame['kurtosis'] = None 
 
     for _, group in frame.groupby('permno'):
@@ -26,5 +22,30 @@ def calculate_kurtosis(frame: pd.DataFrame):
 
     return frame
 
+def sample_month(month: pd.DataFrame):
+    sorted_df = month.sort_values(by='kurtosis')
+
+    # Divide into 10 quantiles 
+    quantiles = pd.qcut(sorted_df['kurtosis'], q=10, labels=False)
+
+    return quantiles
+
 # calculate kurtosis
 df = calculate_kurtosis(load_csv('crsp.csv'))
+df = df.dropna(subset=['kurtosis'])
+
+# Extract unique months
+df['month_year'] = df['date'].dt.to_period('M')
+unique_months = df['month_year'].unique()
+
+# Iterate over unique months
+for month in unique_months:
+    month_data = df[df['month_year'] == month]
+    quantiles = sample_month(month_data)
+
+    # Iterate over each quantile
+    for quantile in range(10):
+        quantile_data = month_data[quantiles == quantile]
+        print(f"Month: {month}, Quantile: {quantile}")
+        print(quantile_data)  # Replace with your desired operation
+
